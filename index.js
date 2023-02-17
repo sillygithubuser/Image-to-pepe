0e98ce23409f0edbbe4119224612994ddc6c78dd
PROGRAMMER1111 <96684730+sillygithubuser@users.noreply.github.com>
2023-02-17 19:57:23 +0900

    Add files via upload
    THIS IS WHAT YOU NEED

---
+++ index.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    let imgs = document.querySelectorAll('img');
+
+    imgs.forEach((a, i) => {
+        a.src = 'https://ichef.bbci.co.uk/news/640/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg'
+        //래액트 덕후이면 쓰는 코드:
+        //a.src = 
+    })
+
+},500)
