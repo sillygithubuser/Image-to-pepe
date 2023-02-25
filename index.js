746cba7c9013e118588eff5902d10d24d4020272
PROGRAMMER1111 <96684730+sillygithubuser@users.noreply.github.com>
2023-02-25 13:54:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ setInterval(() => {
 
     imgs.forEach((a, i) => {
         a.src = 'https://ichef.bbci.co.uk/news/640/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg'
-        //래액트 덕후이면 쓰는 코드:
-        //a.src = 
+        //래액트 덕후이면 쓰는 코드(리액트 사진임ㅋㅋㅋ):
+        //a.src = https://www.patterns.dev/img/reactjs/react-logo@3x.svg
     })
 
 },500)
